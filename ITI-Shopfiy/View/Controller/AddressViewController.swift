475cbf98dc41e87dd659c8bcbf7147c427c34678
ahmed maamoun <amaamoun74@gmail.com>
2023-02-26 21:35:04 +0200

    setup address

--- ITI-Shopfiy/View/Controller/AddressViewController.swift
+++ ITI-Shopfiy/View/Controller/AddressViewController.swift
@@ -6,24 +6,107 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+class AddressViewController: UIViewController , CLLocationManagerDelegate {
+   
+    @IBOutlet weak var mabView: MKMapView!
+    private var locationManager = CLLocationManager()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest // accuracy best is not better for battery
+            locationManager.allowsBackgroundLocationUpdates = true
+            configureAuthority()
+            
+            
+        }
+        func configureAuthority(){
+            DispatchQueue.global().async { [self] in
+                if isLocationServiceEnabled()
+                {
+                    checkAuthorization()
+                }
+                else {
+                    showAlert(msg: "Please enable location service", type: "locationService")
+                }
+            }
+        }
+        func isLocationServiceEnabled() -> Bool{
+            return CLLocationManager.locationServicesEnabled()
+        }
 
-class AddressViewController: UIViewController {
+        func checkAuthorization(){
+            switch locationManager.authorizationStatus {
+            case .notDetermined:
+                locationManager.requestAlwaysAuthorization()
+            break
+            case .authorizedWhenInUse:
+                locationManager.startUpdatingLocation()
+                mabView.showsUserLocation = true
+                break
+            case .authorizedAlways:
+                locationManager.startUpdatingLocation()
+                mabView.showsUserLocation = true
+                break
+                
+            case .denied:
+                showAlert(msg: "Please authorize access to location", type: "authSettings")
+                break
+            case .restricted:
+                showAlert(msg: "Authorization restricted", type: "default")
+                break
+            default:
+                print ("default state of map")
+            }
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.last {
+                print("Location: \(location.coordinate)")
+                zoomToUserLocation(location: location)
+            }
+            locationManager.stopUpdatingLocation()
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            switch status {
+            case .authorizedWhenInUse:
+                locationManager.startUpdatingLocation()
+                mabView.showsUserLocation = true
+                break
+            case .authorizedAlways:
+                locationManager.startUpdatingLocation()
+                mabView.showsUserLocation = true
+                break
+                
+            case .denied:
+                showAlert(msg: "Please authorize access to location", type: "authSettings")
+                break
+            default:
+                print ("default state of map")
+            }
+        }
+        
+        func zoomToUserLocation(location : CLLocation)
+        {
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 750, longitudinalMeters: 750)
+            mabView.setRegion(region, animated: true)
+        }
+        
+        func showAlert(msg: String , type: String) {
+            let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "close", style: .cancel))
+            alert.addAction(UIAlertAction(title: "settings", style: .default , handler: { action in
+                if type == "locationService"
+                {
+                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                }
+                else if type == "authSettings" {
+                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                }
+            }))
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
--- ITI-Shopfiy/View/Controller/PaymentViewController.swift
+++ ITI-Shopfiy/View/Controller/PaymentViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class PaymentViewController: UIViewController {
 
+    @IBOutlet weak var paymentView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        paymentView.isUserInteractionEnabled = false
+        paymentView.alpha = 0.3
+        paymentView.backgroundColor = UIColor.gray
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func GooglePay(_ sender: Any) {
+    }
+    @IBAction func applePay(_ sender: Any) {
+    }
+    
+    @IBAction func cashOnDelivery(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
