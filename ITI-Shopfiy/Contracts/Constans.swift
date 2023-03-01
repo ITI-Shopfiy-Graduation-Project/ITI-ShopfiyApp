6c0bb033ed5615debacf97bafdd305f30c260e2c
ahmed maamoun <amaamoun74@gmail.com>
2023-03-01 19:35:48 +0200

    add core data

---
+++ ITI-Shopfiy/Contracts/Constans.swift
@@ -0,0 +1,15 @@
+//
+//  Constans.swift
+//  ITI-Shopfiy
+//
+//  Created by ahmed on 01/03/2023.
+//
+
+import Foundation
+
+class Constants {
+    
+    private init(){}
+    static var ProductEntity = "Product"
+    
+}
