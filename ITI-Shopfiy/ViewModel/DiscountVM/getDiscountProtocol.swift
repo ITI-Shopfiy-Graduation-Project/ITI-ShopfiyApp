6f99f57193788e3e310a53ec2d406e2d0b138bd8
abdallah <abdallahismailbmx6@gmail.com>
2023-03-04 21:33:25 +0200

    ads code in home screen

---
+++ ITI-Shopfiy/ViewModel/DiscountVM/getDiscountProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  getDiscountProtocol.swift
+//  ITI-Shopfiy
+//
+//  Created by Abdallah ismail on 04/03/2023.
+//
+
+import Foundation
+protocol getDiscountProtocol{
+    func getDiscount()
+}
