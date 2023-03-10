8b153198209f21f4d4c0c07b726807ea6934451d
abdallah <abdallahismailbmx6@gmail.com>
2023-03-10 09:10:38 +0200

    product price  slider

--- ITI-Shopfiy/View/Controller/ProductsViewController.swift
+++ ITI-Shopfiy/View/Controller/ProductsViewController.swift
@@ -11,9 +11,19 @@ import CoreData
 import Foundation
 
 class ProductsViewController: UIViewController{
+    @IBOutlet weak var priceValue: UILabel!
+    @IBOutlet weak var priceSlider: UISlider!
     @IBOutlet weak var like_btn: UIBarButtonItem!
     @IBOutlet weak var cart_btn: UIBarButtonItem!
     
+    @IBAction func showSlider(_ sender: Any) {
+        
+        priceSlider.isHidden = !priceSlider.isHidden
+        priceValue.isHidden =  !priceValue.isHidden
+//        priceSlider.minimumValue = 0
+//        priceSlider.maximumValue = 100
+//        "Price: " + String(Int(sender.value))
+    }
     @IBOutlet weak var productSearchBar: UISearchBar!{
         didSet{
             productSearchBar.delegate = self
@@ -27,6 +37,7 @@ class ProductsViewController: UIViewController{
         }
     }
     
+  
     var productsArray: [Products]? = []
     var searchArray: [Products]? = []
     var productsVM: ProductsVM?
@@ -36,13 +47,18 @@ class ProductsViewController: UIViewController{
     var url: String?
     var vendor: String?
     var indicator: UIActivityIndicatorView?
+    
+    
+    
+
 
 //    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 //    var dataCaching: IDataCaching = DataManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        priceSlider.isHidden = true
+        priceValue.isHidden = true
         indicator = UIActivityIndicatorView(style: .large)
         indicator?.center = view.center
         view.addSubview(indicator ?? UIActivityIndicatorView() )
@@ -127,7 +143,7 @@ extension ProductsViewController: UICollectionViewDataSource, UICollectionViewDe
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCollectionViewCell
         let product = self.productsArray?[indexPath.row]
-        cell.productTitle.text = product?.title ?? ""
+        cell.productTitle.text = product!.variants?[0].price ?? ""
         let productimg = URL(string:product?.image?.src ?? "https://apiv2.allsportsapi.com//logo//players//100288_diego-bri.jpg")
         cell.productImageview?.kf.setImage(with:productimg)
         cell.productsView = self
@@ -268,4 +284,25 @@ extension ProductsViewController{
     
     
 }
+extension ProductsViewController {
+    
+    @IBAction func priceActionSlider(_ sender: UISlider) {
+                priceSlider.maximumValue = 300
+        priceValue.text =  "Price: " + String(Int(sender.value))
+        if sender.value < 150 {
+            productsArray = searchArray!.filter({ Products in
+                Double(Products.variants?[0].price ?? "0")! < Double(sender.value)
+                
+                
+            })}
+        if sender.value > 150 {
+            productsArray = searchArray!.filter({ Products in
+                Double(Products.variants?[0].price ?? "0")! >  Double(sender.value)
+                
+            })
+    }
+    self.productsCollectionView.reloadData()
 
+    }
+            }
+                                                     
