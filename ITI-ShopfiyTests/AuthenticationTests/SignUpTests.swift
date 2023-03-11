ff7f53bf628e870421d0f0e85acb9bb5c041841c
Fadysameh2020 <fadysameh10@gmail.com>
2023-03-11 15:30:13 +0200

    Unit Testing

---
+++ ITI-ShopfiyTests/AuthenticationTests/SignUpTests.swift
@@ -0,0 +1,85 @@
+//
+//  SignUpTests.swift
+//  ITI-ShopfiyTests
+//
+//  Created by MESHO on 11/03/2023.
+//
+
+import XCTest
+@testable import ITI_Shopfiy
+
+final class SignUpTests: XCTestCase {
+
+    var registerAuthentication: registerProtocol?
+    
+    var customer1Email: String?
+    var customer1Password: String?
+    
+    var customer2Email: String?
+    var customer2Password: String?
+    
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        registerAuthentication = RegisterVM()
+        
+        customer1Email = "fedo"
+        customer1Password = "123"
+        
+        customer2Email = "abdo22@iti.com"
+        customer2Password = "12345"
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        registerAuthentication = nil
+        
+        customer1Email = ""
+        customer1Password = ""
+        
+        customer2Email = ""
+        customer2Password = ""
+    }
+    
+    
+    func testAuthentication1(){
+        let result = true
+        if result == registerAuthentication?.isValidEmail(customer1Email ?? "fedo"){
+            XCTAssertTrue(result == true)
+        }else{
+            XCTFail("Fail with error")
+        }
+    }
+
+    func testAuthentication2(){
+        let result = true
+        if result == registerAuthentication?.isValidEmail(customer2Email ?? "abdo22@iti.com"){
+            XCTAssertTrue(result == true)
+        }else{
+            XCTFail("Fail with error")
+        }
+    }
+    
+    func testAuthentication3(){
+        let result = true
+        if result == registerAuthentication?.isValidPassword(password: customer1Password ?? "123", confirmPassword: "12"){
+            XCTAssertTrue(result == true)
+        }else{
+            XCTFail("Fail with error")
+        }
+    }
+
+    func testAuthentication4(){
+        let result = true
+        if result == registerAuthentication?.isValidPassword(password: customer2Password ?? "12345", confirmPassword: customer2Password ?? "12345"){
+            XCTAssertTrue(result == true)
+        }else{
+            XCTFail("Fail with error")
+        }
+    }
+    
+    
+
+    
+    
+}
---
+++ ITI-ShopfiyTests/NetworkTests/AddressTests.swift
@@ -0,0 +1,77 @@
+//
+//  AddressTests.swift
+//  ITI-ShopfiyTests
+//
+//  Created by MESHO on 11/03/2023.
+//
+
+import XCTest
+@testable import ITI_Shopfiy
+
+final class AddressTests: XCTestCase {
+    
+    var user1_Id: Int?
+    var user2_Id: Int?
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        user1_Id = 123
+        user2_Id = 6874931691801
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+    func testFetchuserAddresses() throws{
+        let expectaion = expectation(description: "Waiting for the API to get Addresses")
+        AddressNetwork.sharedInstance.fetchAllUserAddresses(userId: user1_Id ?? 123) {  result in
+            if result != nil {
+                guard let addresses = result?.addresses else
+                {
+                    XCTFail("No Data")
+                    XCTAssertNil(result)
+                    expectaion.fulfill()
+                    return
+                }
+                XCTAssertNotNil(addresses)
+                XCTAssertNotEqual(addresses.count, 0)
+                expectaion.fulfill()
+            }else{
+                XCTFail("please check your Email or Password")
+                XCTFail("failed to get your addresses in app")
+                expectaion.fulfill()
+                return
+            }
+        }
+        waitForExpectations(timeout: 3 , handler: nil)
+    }
+    
+    func testFetchuserAddresses2() throws{
+        let expectaion = expectation(description: "Waiting for the API to get Addresses")
+        AddressNetwork.sharedInstance.fetchAllUserAddresses(userId: user2_Id ?? 6874931691801) {  result in
+            if result != nil {
+                guard let addresses = result?.addresses else
+                {
+                    XCTFail("No Data")
+                    XCTAssertNil(result)
+                    expectaion.fulfill()
+                    return
+                }
+                XCTAssertNotNil(addresses)
+                XCTAssertNotEqual(addresses.count, 0)
+                expectaion.fulfill()
+            }else{
+                XCTFail("please check your Email or Password")
+                XCTFail("failed to get your addresses in app")
+                expectaion.fulfill()
+                return
+            }
+        }
+        waitForExpectations(timeout: 3 , handler: nil)
+    }
+    
+    
+
+}
